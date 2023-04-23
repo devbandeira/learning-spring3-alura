47713fac34e81adf8356e44a190c373dc817d591
mempet <mempet2022@gmail.com>
2023-04-23 19:56:59 -0300

    Fim aula Ordenacao

--- src/main/java/alura/spring/cursoSpring/controller/MedicoController2.java
+++ src/main/java/alura/spring/cursoSpring/controller/MedicoController2.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -48,7 +49,7 @@ public class MedicoController2 {
     }
 
     @GetMapping//n preciso usar a anotation @Transitional pois eu nao estou escrevendo ou excluindo algo no BD, somente lendo
-    public /*mudar o List<> para PAGE<> que contem informacao sobre paginacao alem dos dados de medico*/Page<DadosListagemMedico> listar(Pageable paginacao/*importar ela do pacote do spring para fazer a paginacao*/){/*Se eu deixei no generics <Medico> Vou retornar tudo do medico e nao e para retornar tudo
+    public /*mudar o List<> para PAGE<> que contem informacao sobre paginacao alem dos dados de medico*/Page<DadosListagemMedico> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao/*importar ela do pacote do spring para fazer a paginacao*/){/*Se eu deixei no generics <Medico> Vou retornar tudo do medico e nao e para retornar tudo
         //..da mesma forma que criamos um DTO para DadosCadastroMedico/Paciente, vamos criar um DTO para devolver dados da API, entao sera um <DadosListagemMEdico>
         /*return repository.findAll();*/
         //Apos criar o DTO, o repository.findall() dara erro, pois findAll retorna uma lista de medico, a antidade JPA, mas o nosso retorno e DadosListagemMedico, entao vou ter que converter. Converter de medico para DadosListagemMEdico*/
@@ -67,3 +68,14 @@ public class MedicoController2 {
 
 /*lembrar de fazer o retorno do controller retornar um page e nao uma lista, pois assim devolveremos informacoes sobre a paginacao tambem. Que sera mt util para o front*/
 /*Para controlar o numero de registros que queremos devolver, devo passar na URL apos "?size=1" para retornar 1 registro e para paginar apos o size passo "&page=2" */
+
+/** ordenacao***/
+/*Para fazer ordenacao eu passo na URL tbm "?sort=nome", neste caso esta ordenando pelo campo nome do meu objeto json retornado na paginacao.*/
+/*por padrao a ordenacao e de forma crescente, para mudar, posso passar na url tbm "?sorte=crm,desc" desc de decrescente e asc para ascendente (crescente)*/
+/*por padrao o sort, sizem page, etc sao em ingles. Mas conseguimos mudar la "Customizacao de parametros application.properties"*/
+/* @PageableDefault(size = 10, page = 0, sort = {"nome"} */
+/*esse PageableDefault e utilizado caso n seja passado nada na url*/
+
+/*PARA SABER COMO ELE TA FAZENDO A PAGINACAO E A ORDENACAO E EU QUERO VER ISSO NO CONSOLE, VER O LOG SQL QUE ELE ESTA USANDO PARA FAZER ESSA QUERY*/
+/*POSSO FAZER ESSA CONFIG NO application.properties (spring.jpa.show-sql=true)*/
+/*formatar o comando sql JPA que vai aparecer no console (spring.jpa.properties.hibernate.format_sql=true)*/
